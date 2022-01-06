9791de5a28e6b9af99f906f2d15f36995c1e69d8
gly <glefranc@eig.fr>
2022-01-06 18:26:13 +0100

    WIP on unord set déclaration

--- include/cstlike/unordered_set.h
+++ include/cstlike/unordered_set.h
@@ -9,4 +9,22 @@ typedef struct cstl_unordered_set_t {
     cstl_vector buffer;
 } cstl_unordered_set;
 
+/// cstl_unordered_set constructor and destructor
+cstl_unordered_set* unordered_set_new(size_t size_item, deleter_t deleter);
+void unordered_set_init(cstl_unordered_set* set, size_t size_item, deleter_t);
+void unordered_set_clear(cstl_unordered_set* set);
+void unordered_set_free(cstl_unordered_set* set);
+
+/// Modifiers
+void unordered_set_resize(cstl_unordered_set* set, size_t nb_items);
+void unordered_set_reserve(cstl_unordered_set* set, size_t nb_items);
+void unordered_set_shrink_to_fit(cstl_unordered_set* set);
+void unordered_set_insert_from(cstl_unordered_set* set, void*);
+void unordered_set_insert_inplace(cstl_unordered_set* set, void (*init_value_fn)(void*));
+
+
+/// Accessors
+void* unordered_set_find(cstl_unordered_set* set, void*);
+void* unordered_set_find_if(cstl_unordered_set* set, bool (*compare_fn)(void*, void*));
+
 #endif
